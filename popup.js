cad88b51a8d1eae27eabdcc56de75b398193a5b6
ljh <mojijanhao@sofyun.com>
2017-12-26 13:06:21 +0800

    修改popup页面通讯方法，同步开启关闭状态

--- popup.js
+++ popup.js
@@ -8,82 +8,76 @@
 // user devices.
 'use strict';
 
-window.onload = state_;
-
-let tabId = 0;
-
 // 发送消息的type
 const typeList = {
   previewAndSubmit: 'previewAndSubmit',
   cancelAll: 'cancelAll',
-  toggleBar: 'toggleBar'
+  toggleBar: 'toggleBar',
+  getStatus: 'getStatus'
 }
 
 // 监听是否开启或关闭状态
 chrome.runtime.onMessage.addListener(switchEventHandle);
 
-// 加载完页面后立刻加载的方法
-function state_() {
-  let className = localStorage.getItem("status_className");
-  let text = localStorage.getItem("status_text");
-  if (className && text) {
-    document.getElementById('status_text').innerHTML = text;
-    document.getElementById('status_icon').className = className;
-  }
-}
+runtimeSendMsg(typeList.getStatus);
 
 // 点击提交打开提交框
 function messageBox () {
-  if (tabId === 0) {
-    sendMessage_(typeList.previewAndSubmit);
-    return;
-  }
-  chrome.tabs.sendMessage(tabId, {type: typeList.previewAndSubmit});
+  sendMessage_(typeList.previewAndSubmit);
 }
 
 // 点击全部取消
 function cancel () {
-  if (tabId === 0) {
-    sendMessage_(typeList.cancelAll);
-    return;
-  }
-  chrome.tabs.sendMessage(tabId, {type: typeList.cancelAll});
+  sendMessage_(typeList.cancelAll);
 }
 
 // 发送事件
 function sendMessage_(type) {
   chrome.tabs.query({active: true, currentWindow: true}, (tab) => {
-    tabId = tab[0].id;
     chrome.tabs.sendMessage(tab[0].id, {type});
   });
 }
 
-// 修改点击开启关闭的ui状态
+// 发送runtime事件
+function runtimeSendMsg(type) {
+  let message = {
+    type
+  };
+  chrome.tabs.query({active: true, currentWindow: true}, (tab) => {
+    if (type === typeList.getStatus) {
+      message.tabId = tab[0].id;
+      message.windowId = tab[0].windowId;
+    }
+    chrome.runtime.sendMessage(message);
+  });
+}
+
+// 监听事件
 function switchEventHandle(mess) {
-  let icon = document.getElementById('status_icon');
-  let text = document.getElementById('status_text');
-  if (mess.type === 'open') {
-    icon.className = 'status open';
-    text.innerHTML = '开启';
-    localStorage.setItem('status_text', '开启')
-    localStorage.setItem('status_className', 'status open')
-  }
-  if (mess.type === 'close'){
-    icon.className = 'status close';
-    text.innerHTML = '关闭'
-    localStorage.removeItem("status_text");
-    localStorage.removeItem("status_className");
+  console.log(mess.type);
+  switch (mess.type) {
+    case 'reponseStatus':
+      changeStatus(mess.status);
+      break;
   }
 }
 
+// 修改点击开启关闭的ui状态
+function changeStatus(status) {
+  let icon = document.getElementById('status_icon');
+  let text = document.getElementById('status_text');
+  icon.className = status ? 'status open' : 'status close';
+  text.innerHTML = status ? '开启' : '关闭';
+}
+
 // 点击开启或关闭按钮
-function changeStatus() {
-  sendMessage_(typeList.toggleBar)
+function switchStatus() {
+  sendMessage_(typeList.toggleBar);
 }
 
 // 为按钮添加点击事件
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('submit').addEventListener('click', messageBox);
   document.getElementById('cancel').addEventListener('click', cancel);
-  document.getElementById('status').addEventListener('click', changeStatus);
+  document.getElementById('status').addEventListener('click', switchStatus);
 });
